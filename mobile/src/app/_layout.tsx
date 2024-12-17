caa2b4051d43b01474f7f586f9691a6a5f11c85a
Bruno S. Duarte <brunosduarte@hotmail.com>
2024-12-17 19:17:45 -0300

    feat: welcome component

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -19,7 +19,7 @@ export default function Layout() {
     Rubik_700Bold
   })
 
-  if(fontsLoaded) {
+  if(!fontsLoaded) {
     return <Loading />
   }
 
--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,13 +1,10 @@
+import Welcome from "@/components/welcome";
 import { Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-        <Text style={{ fontSize: 22 }}>Hello World</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+        <Welcome />
     </View>
   );
 }
